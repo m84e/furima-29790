01cd4b8c61fce74a48f879bb3caff21a6a514cd0
m84e <ryokenyout@gmail.com>
2020-10-16 14:52:46 +0900

    商品購入機能3

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,33 +9,33 @@ class OrdersController < ApplicationController
   end
 
   def create
-    payjp
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      payjp
+      @order_address.save
+        return redirect_to root_path
+    else
+      render 'index'
+    end
   end
 
+
   private
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :prefectures_id, :city, :house_num, :build_name, :phone_num).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
+  end
 
-    def order_address_params
-      params.require(:order_address).permit(:postal_code, :prefectures_id, :city, :house_num, :build_name, :phone_num).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
-    end
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
-    def set_item
-      @item = Item.find(params[:item_id])
-    end
-
-    def payjp
-        @order_address = OrderAddress.new(order_address_params)
-      if @order_address.valid?
-        Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
-        Payjp::Charge.create(
-          amount: @item.price,
-          card: order_address_params[:token],
-          currency: 'jpy'
-        )
-        @order_address.save
-        return redirect_to root_path
-      else
-        render 'index'
-      end
-    end
+  def payjp
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_address_params[:token],
+      currency: 'jpy'
+    )
+  end
 
 end
